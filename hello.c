2c2491c681a74e854ff000584eb8a2f6bba87c61
Zhusb90 <49397290+Zhusb90@users.noreply.github.com>
2020-07-16 11:43:50 +0800

    add hello.c
    add the very first c file.

---
+++ hello.c
@@ -0,0 +1,15 @@
+/* project:         a test file                 */
+/* brief:           just for test               */
+/* auther:          zhusb                       */
+/* date:            2020-07-16                  */
+/* description:     None                        */
+/************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n")
+    printf("anther:zhusb\n")
+    printf("date:2020-07-16\n")
+}
